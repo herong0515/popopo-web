e3ef4257aa6576cb204595aa33e543326b7b6eef
herong0515 <102296873+herong0515@users.noreply.github.com>
2023-03-20 20:43:15 +0900

    :wrench: amplify add auth and analytics

--- popopo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ popopo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,27 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "popopo": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "api": {
+    "popopo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "popopo82fd06a2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
